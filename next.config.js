34ee8bc457cb87a28d7e8ead5833c066ead5677a
omarhash95 <omar@undefined.nyc>
2025-10-21 18:31:20 -0400

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
-  webpack: (config) => {
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  experimental: {
+    // Disable incremental cache to prevent EIO errors
+    isrMemoryCacheSize: 0,
+  },
+  webpack: (config, { isServer }) => {
     config.cache = false;
+    // Prevent filesystem watching issues
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules', '**/.next'],
+    };
     return config;
   },
 };
+
 module.exports = nextConfig;
